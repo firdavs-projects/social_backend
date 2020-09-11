d21633483ecf22d43a5ec8baedcc7eb0e4a04650
Firdavs_Abdulloev <firdavs.abdulloev.7725@gmail.com>
2020-09-11 23:59:06 +0500

    v8.0 Docker toolbox dont working im using mysql in my system

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 const http = require('http');
 const mysqlx = require('@mysql/xdevapi');
-const { table } = require('console');
 
 const port = process.env.PORT || 9999;
 const statusOk = 200;
@@ -12,187 +11,208 @@ const statusNotFound = 404;
 const statusInternalServerError = 500;
 const schema = 'social';
 
-let nextId = 1;
-const posts = [];
-
 const client = mysqlx.getClient({
-    user: 'app',
-    password: 'pass',
-    host: '0.0.0.0',
-    port: 3306
+  user: 'app',
+  password: 'pass',
+  host: '0.0.0.0',
+  port: 33060
 });
 
-function sendResponse(response, { status = statusOk, headers = {}, body = null }) {
-    Object.entries(headers).forEach(function ([key, value]) {
-        response.setHeader(key, value);
-    });
-    response.writeHead(status);
-    response.end(body);
+function sendResponse(response, {status = statusOk, headers = {}, body = null}) {
+  Object.entries(headers).forEach(function ([key, value]) {
+    response.setHeader(key, value);
+  });
+  response.writeHead(status);
+  response.end(body);
 }
 
 function sendJSON(response, body) {
-    sendResponse(response, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body),
-    });
+  sendResponse(response, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
 }
 
 function map(columns) {
-    return row => row.reduce((res, value, i) => ({ ...res, [columns[i].getColumnLabel()]: value }), {});
+  return row => row.reduce((res, value, i) => ({...res, [columns[i].getColumnLabel()]: value}), {});
 }
 
 const methods = new Map();
-methods.set('/posts.get', async ({ response, db }) => {
-    const table = await db.getTable('posts');
-    const result = await table.select(['id', 'content', 'likes', 'created'])
-        .orderBy('created DESC')
-        .execute();
-    const data = result.fetchAll();
-    result.getAffectedItemsCount();
-    const columns = result.getColumns();
-    const posts = data.map(columns);
-    sendJSON(response, posts);
+
+methods.set('/posts.get', async ({response, db}) => {
+  const table = await db.getTable('posts');
+  const result = await table.select(['id', 'content', 'likes', 'created'])
+    .orderBy('created DESC')
+    .execute();
+
+  const data = result.fetchAll();
+  const columns = result.getColumns();
+  const posts = data.map(map(columns));
+  sendJSON(response, posts);
 });
 
-methods.set('/posts.getById', function ({ response, searchParams }) {
-    const id = searchParams.get('id');
-    if (!searchParams.has('id') | isNaN(Number(id)) | id === '') {
-        sendResponse(response, { status: statusBadRequest });
-        return;
-    }
-    const post = posts.find(el => el.id === Number(id));
-    if (!post) {
-        sendResponse(response, { status: statusNotFound });
-        return;
-    } else {
-        if (post.removed === true) {
-            sendResponse(response, { status: statusNotFound });
-            return;
-        }
-    }
-    sendJSON(response, post);
+methods.set('/posts.getById', function ({response, searchParams}) {
+  if (!searchParams.has('id')) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
+
+  const id = Number(searchParams.get('id'));
+  if (Number.isNaN(id)) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
+
+  const post = posts.filter(o => !o.removed).find(o => o.id === id);
+  if (post === undefined) {
+    sendResponse(response, {status: statusNotFound});
+    return;
+  }
+
+  sendJSON(response, post);
 });
 
-methods.set('/posts.post', function ({ response, searchParams }) {
-    const content = searchParams.get('content');
-    if (!searchParams.has('content') | content === '') {
-        sendResponse(response, { status: statusBadRequest });
-        return;
-    }
-    const post = {
-        id: nextId++,
-        content: content,
-        created: Date.now(),
-        removed: false,
-    };
-    posts.unshift(post);
-    sendJSON(response, post);
+methods.set('/posts.post', function ({response, searchParams}) {
+  if (!searchParams.has('content')) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
+
+  const content = searchParams.get('content');
+
+  const post = {
+    id: nextId++,
+    content: content,
+    removed: false,
+    created: Date.now(),
+  };
+
+  posts.unshift(post);
+  sendJSON(response, post);
 });
 
-methods.set('/posts.edit', function ({ response, searchParams }) {
-    const id = searchParams.get('id');
-    const content = searchParams.get('content');
-    if (!searchParams.has('id') | isNaN(Number(id)) | id === '' | !searchParams.has('content') | content === '') {
-        sendResponse(response, { status: statusBadRequest });
-        return;
-    }
-    const post = posts.find(el => el.id === Number(id));
-    if (!post) {
-        sendResponse(response, { status: statusNotFound });
-        return;
-    } else {
-        if (post.removed === true) {
-            sendResponse(response, { status: statusNotFound });
-            return;
-        }
-    }
-    const index = posts.findIndex(o => o.id === Number(id));
-    posts[index].content = content;
-    sendJSON(response, posts[index]);
+methods.set('/posts.edit', function ({response, searchParams}) {
+  if (!searchParams.has('id')) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
+
+  const id = Number(searchParams.get('id'));
+  if (Number.isNaN(id)) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
+
+  if (!searchParams.has('content')) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
+  const content = searchParams.get('content');
+
+  const post = posts.filter(o => !o.removed).find(o => o.id === id);
+  if (post === undefined) {
+    sendResponse(response, {status: statusNotFound});
+    return;
+  }
+
+  post.content = content;
+
+  sendJSON(response, post);
 });
 
-methods.set('/posts.delete', async ({ response, searchParams, db }) => {
-    if (!searchParams.has('id')) {
-        sendResponse(response, { status: statusBadRequest });
-        return;
-    }
+methods.set('/posts.delete', async ({response, searchParams, db}) => {
+  if (!searchParams.has('id')) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
 
-    const id = Number(searchParams.get('id'));
-    if (Number.isNaN(id)) {
-        sendResponse(response, { status: statusBadRequest });
-        return;
-    }
+  const id = Number(searchParams.get('id'));
+  if (Number.isNaN(id)) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
 
-    const table = await db.getTable('posts');
-    const result = await table.update()
-        .set('removed', true)
-        .where('id=:id')
-        .bind('id', id)
-        .execute();
-    const removed = result.getAffectedItemsCount();
+  const table = await db.getTable('posts');
+  const result = await table.update()
+    .set('removed', true)
+    .where('id = :id')
+    .bind('id', id)
+    .execute();
 
-    if (removed === 0) {
-        sendResponse(response, { status: statusNotFound });
-        return;
-    }
-    sendResponse(response, { status: statusNoContent });
+  const removed = result.getAffectedItemsCount();
+
+  if (removed === 0) {
+    sendResponse(response, {status: statusNotFound});
+    return;
+  }
+
+  sendResponse(response, {status: statusNoContent});
 });
 
-methods.set('/posts.restore', function ({ response, searchParams }) {
-    const id = searchParams.get('id');
-    if (!searchParams.has('id') | isNaN(Number(id)) | id === '') {
-        sendResponse(response, { status: statusBadRequest });
-        return;
-    }
-    const post = posts.find(el => el.id === Number(id));
-    if (!post) {
-        sendResponse(response, { status: statusNotFound });
-        return;
-    }
-    const index = posts.findIndex(o => o.id === Number(id));
-    if (post.removed === true) {
-        posts[index].removed = false;
-        sendJSON(response, posts[index]);
-        return;
-    }
-    sendResponse(response, { status: statusBadRequest });
+methods.set('/posts.restore', function ({response, searchParams}) {
+  if (!searchParams.has('id')) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
+
+  const id = Number(searchParams.get('id'));
+  if (Number.isNaN(id)) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
+
+  const post = posts.find(o => o.id === id);
+  if (post === undefined) {
+    sendResponse(response, {status: statusNotFound});
+    return;
+  }
+
+  if (post.removed !== true) {
+    sendResponse(response, {status: statusBadRequest});
+    return;
+  }
+
+  post.removed = false;
+
+  sendJSON(response, post);
 });
 
 const server = http.createServer(async (request, response) => {
-    const { pathname, searchParams } = new URL(request.url, `http://${request.headers.host}`);
+  const {pathname, searchParams} = new URL(request.url, `http://${request.headers.host}`);
 
-    const method = methods.get(pathname);
-    if (method === undefined) {
-        sendResponse(response, { status: statusNotFound });
-        return;
+  const method = methods.get(pathname);
+  if (method === undefined) {
+    sendResponse(response, {status: statusNotFound});
+    return;
+  }
+
+  let session = null;
+  try {
+    session = await client.getSession();
+    const db = await session.getSchema(schema);
+
+    const params = {
+      request,
+      response,
+      pathname,
+      searchParams,
+      db,
+    };
+
+    await method(params);
+  } catch (e) {
+    sendResponse(response, {status: statusInternalServerError});
+  } finally {
+    if (session !== null) {
+      try {
+        await session.close();
+      } catch (e) {
+        console.log(e);
+      }
     }
-    let session = null;
-    try {
-        session = await client.getSession();
-        const db = await session.getschema(schema);
-
-        const params = {
-            request,
-            response,
-            pathname,
-            searchParams,
-            db,
-        };
-
-        await method(params);
-    } catch (e) {
-        sendResponse(response, { status: statusInternalServerError });
-    } finally {
-        if (session !== null) {
-            try {
-                await session.close();
-            } catch (e) {
-                console.log(e);
-            }
-        }
-    }
+  }
 });
 
 server.listen(port);
